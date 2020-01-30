9f6abc5b57d824d50d97114b657827db30312411
Quervin <Quervin@QUERVIN-PC>
2020-01-29 20:56:46 -0600

    Edited API

---
+++ Domain/Data/Entities/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Data.Entities
+{
+    public class Estudiante
+    {
+        public long EstudianteId { get; set; }
+        public string Nombre { get; set; }
+        public string Primer_Apellido { get; set; }
+        public string Segundo_Apellido { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/EstudianteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Domain.Data.Entities;
+using System.Data;
+
+namespace WebAPI.Controllers
+{
+    public class EstudianteController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+
+            string query = @
+        }
+    }
+}
